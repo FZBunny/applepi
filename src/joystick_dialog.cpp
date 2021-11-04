43a0fddeccc7e45991b21f733e5a0bf86c47795a
FZBunny <j.bruce.ward@gmail.com>
2021-11-04 17:00:39 -0300

    Reworked & more-or-less finished 'joystickDialog' class.

--- src/joystick_dialog.cpp
+++ src/joystick_dialog.cpp
@@ -1,6 +1,36 @@
+/*****************************************************************************
+
+    File: "joystick_dialog.cpp"
+
+    Copyright (C) 2021, Bruce Ward
+
+    This file is part of applepi, an apple2 emulator for Linux,
+    especially aimed at the Raspberry Pi operating system.
+
+    applepi is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    applepi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with applepi; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Or perhaps visit: http://www.gnu.org/licenses/licenses.html
+
+*****************************************************************************/
 
 
+#include <QTextStream>
+
+#include "defs.h"
 #include "joystick_dialog.h"
+
 
 
 joystickDialog::joystickDialog (MainWindow* parent)
@@ -8,13 +38,93 @@ joystickDialog::joystickDialog (MainWindow* parent)
     m_parent = parent ;
 
     int width = 360 ;
-    int height = 360 ;
+    int height = 170 ;
     int x, y ;
     CENTER_DIALOG
+    setWindowTitle ("Select Controller") ;
 
-    m_list = new QListWidget (this) ;
-    m_list->move (10,10) ;
-    m_list->resize (340,80) ;
+    m_listWidget = new QListWidget (this) ;
+    m_listWidget->move (20,20) ;
+    m_listWidget->resize (320,80) ;
 
+    m_saveButton = new QPushButton ("Save", this) ;
+    m_saveButton->move (260,130) ;
+    m_cancelButton = new QPushButton ("Cancel", this) ;
+    m_cancelButton->move (20,130) ;
+
+    CFG->Get ("game_controller_name", &m_currentControllerName) ;
+
+    m_controllerIDs = QGamepadManager::instance()->connectedGamepads() ;
+    gameController* controller ;
+
+    for (int i=0; i<m_controllerIDs.length(); i++) {         // Add all gamepads / joystcks to
+        int id = m_controllerIDs.at(i) ;                     // controller list & list widget
+        m_gamepad = new QGamepad (id) ;
+        controller = new gameController() ;
+        controller->type = GAMEPAD ;
+        controller->id = id ;
+        controller->name = m_gamepad->name() ;
+        m_listWidget->addItem (controller->name) ;
+        m_controllerList.append (*controller) ;
+        delete controller ;
+    }
+
+    for (int i=0;;i++) {                                   // Add all mice to lists
+        QString mouseDev ;
+        QTextStream(&mouseDev) << "/dev/input/mouse" << i ;
+        if (QFileInfo::exists(mouseDev)) {
+            m_listWidget->addItem (mouseDev) ;
+            controller = new gameController() ;
+            controller->type = MOUSE ;
+            controller->id = 0 ;
+            controller->name = mouseDev ;
+            m_controllerList.append (*controller) ;
+            delete controller ;
+        } else {
+            break ;
+        }
+    }
+
+    for (int i=0; i<m_controllerList.length(); i++) {      // Highlight current controller on the list
+        if (m_controllerList.at(i).name == m_currentControllerName) {
+            m_listWidget->setCurrentRow (i, QItemSelectionModel::SelectCurrent) ;
+            m_selectedRow = i ;
+            break ;
+        }
+    }
+
+    connect (m_saveButton,   &QPushButton::clicked,     this, &joystickDialog::onSaveButton) ;
+    connect (m_cancelButton, &QPushButton::clicked,     this, &joystickDialog::onCancelButton) ;
+    connect (m_listWidget,   &QListWidget::itemClicked, this, &joystickDialog::onListWidget) ;
+
+    m_saveButton->setEnabled (false) ;
+
+}
+
+
+void joystickDialog::onListWidget (QListWidgetItem *item)
+{
+    m_selectedRow = m_listWidget->currentRow() ;
+    m_selectedID = m_controllerList.at(m_selectedRow).id ;
+    m_selectedControllerName = item->text() ;
+
+    m_saveButton->setEnabled (true) ;
+}
+
+
+void joystickDialog::onSaveButton (void)
+{
+ //   m_selectedControllerName = m_controllerList.at(m_selectedRow).name ;
+
+    CFG->Set ("game_controller_name", m_selectedControllerName) ;
+    CFG->Set ("game_controller_ID", m_selectedID) ;
+
+    this->accept() ;
+}
+
+
+void joystickDialog::onCancelButton (void)
+{
+    this->accept() ;
 }
 
--- src/joystick_dialog.h
+++ src/joystick_dialog.h
@@ -1,3 +1,30 @@
+/*****************************************************************************
+
+    File: "joystick_dialog.h"
+
+    Copyright (C) 2021, Bruce Ward
+
+    This file is part of applepi, an apple2 emulator for Linux,
+    especially aimed at the Raspberry Pi operating system.
+
+    applepi is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    applepi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with applepi; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Or perhaps visit: http://www.gnu.org/licenses/licenses.html
+
+*****************************************************************************/
+
 
 #ifndef JOYSTCK_DIALOG_H
 #define JOYSTCK_DIALOG_H
@@ -11,31 +38,55 @@
 #include <QLabel>
 #include <QListWidget>
 
+#include "mainwindow.h"
 
-//#include <>
-//#include <>
+
+class gameController
+{
+public:
+    gameController() {} ;
+    ~gameController() {} ;
+
+    int type ;
+    int id ;
+    QString name ;
+} ;
+
 
 
 class joystickDialog : public QDialog
 {
 public:
 
+    joystickDialog (MainWindow* parent) ;
+    ~joystickDialog() {} ;
 
 private:
 
-    QListWidget*  m_list ;
+    void onSaveButton (void) ;
+    void onCancelButton (void) ;
+    void onMapButton (void) ;
+    void onListWidget (QListWidgetItem *item) ;
+    void openGamepad (void) ;
 
-    QRadioButton* m_mouse ;
-    QRadioButton* m_joystick ;
+    void buttonBChanged(bool value) ;
 
-    QSpinBox*     m_sensitivityBox ;
-    QSpinBox*     m_deadbandBox ;
+    const int GAMEPAD = 0 ;
+    const int MOUSE   = 1 ;
 
-    QPushButton*  m_doneButton ;
-    QLabel*       m_lable1 ;
-    QLabel*       m_lable2 ;
-    
+    MainWindow*    m_parent ;
+    QListWidget*   m_listWidget ;
+    QGamepad*      m_gamepad ;
+    QString        m_currentControllerName ;
+    QString        m_selectedControllerName ;
+    int            m_selectedRow ;
+    uint           m_selectedID ;
 
+    QList<int>             m_controllerIDs ;
+    QList<gameController>  m_controllerList ;
+
+    QPushButton*  m_saveButton ;
+    QPushButton*  m_cancelButton ;
 
 } ;
 
