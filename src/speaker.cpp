1ca3107253148fa58fb60b119825dd02eccddc1b
J.B.Ward <j.bruce.ward@gmail.com>
2022-07-03 11:15:57 -0300

    Yet more fiddling in preparation for windows port.

--- src/speaker.cpp
+++
@@ -1,7 +0,0 @@
-
-#ifdef Q_OS_WINDOWS
-    #include "win-speaker.cpp"
-#else
-    #include "lin-speaker.cpp"
-#endif
-
--- src/speaker.h
+++
@@ -1,7 +0,0 @@
-
-#ifdef Q_OS_WINDOWS
-    #include "win-speaker.h"
-#else
-    #include "lin-speaker.h"
-#endif
-
